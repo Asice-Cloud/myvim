f0c9b622a9dbcf90549a196819796b89e3bf5525
asice <2492215094@qq.com>
2024-09-20 23:52:59 +0800

    feat:first commit

---
+++ init.lua
@@ -0,0 +1,336 @@
+-- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
+-- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
+local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+-- 设置字体
+vim.opt.guifont = "JetBrainsMono Nerd Font Mono:h15"
+
+vim.g.neovide_remember_window_size = true
+local VimExtConfig = [[ highlight Normal guibg=NONE ctermbg=None ]]
+vim.cmd(VimExtConfig)
+vim.g.neovide_transparency = 0.65
+
+-- validate that lazy is available
+if not pcall(require, "lazy") then
+  -- stylua: ignore
+  vim.api.nvim_echo(
+    { { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } },
+    true, {})
+  vim.fn.getchar()
+  vim.cmd.quit()
+end
+
+vim.g.airline_extensions_tabline_formatter = "defualt"
+vim.g.airline_section_y = 'BN: %{bufnr("%")}'
+vim.g.airline_theme = "tomorrow"
+
+require "lazy_setup"
+require "polish"
+require "mapping"
+
+require("notify").setup {
+  background_colour = "#000000",
+}
+-- 定义一个函数来执行 CMake 构建流程
+-- 定义一个函数来执行 CMake 构建流程
+local function build_project()
+  -- 创建 build 目录
+  vim.cmd "!mkdir -p build"
+  -- 切换到 build 目录
+  vim.cmd "cd build"
+  -- 运行 cmake
+  vim.cmd "!cmake .."
+  -- 运行 make
+  vim.cmd "!make"
+  vim.cmd "cd .."
+end
+
+-- 创建一个用户命令
+vim.api.nvim_create_user_command("BuildProject", build_project, {})
+
+-- 将 F8 键映射到用户命令
+vim.api.nvim_set_keymap("n", "<F8>", ":BuildProject<CR>", { noremap = true, silent = true })
+
+-- F10 compile file
+
+function CompileAndRunFile()
+  -- 获取当前文件路径、文件名和文件类型
+  local filepath = vim.fn.expand "%:p" -- 获取文件的完整路径
+  local filename = vim.fn.expand "%:t:r" -- 获取不带扩展名的文件名
+  local filetype = vim.bo.filetype
+
+  -- 设置输出目录和生成的可执行文件路径
+  local output_dir = "build/bin"
+  local output_file = string.format("%s/%s", output_dir, filename)
+
+  -- 如果目录不存在，创建它
+  vim.fn.mkdir(output_dir, "p")
+
+  -- 初始化编译和运行命令
+  local compile_cmd = ""
+  local run_cmd = ""
+
+  -- 根据文件类型选择编译器
+  if filetype == "cpp" then
+    compile_cmd = string.format("g++ -std=c++23 -o %s %s -O2", output_file, filepath)
+    run_cmd = string.format("./%s", output_file)
+  elseif filetype == "c" then
+    compile_cmd = string.format("gcc -std=c2x -o %s %s", output_file, filepath)
+    run_cmd = string.format("./%s", output_file)
+  elseif filetype == "lua" then
+    run_cmd = string.format("lua %s", filepath)
+  elseif filetype == "go" then
+    run_cmd = string.format("go run %s", filepath)
+  elseif filetype == "javascript" then
+    run_cmd = string.format("node %s", filepath)
+  elseif filetype == "java" then
+    compile_cmd = string.format("javac -d %s %s", output_dir, filepath)
+    run_cmd = string.format("java -cp %s %s", output_dir, filename)
+  elseif filetype == "python" then
+    run_cmd = string.format("python3 %s", filepath)
+  else
+    vim.notify("Unsupported filetype: " .. filetype, vim.log.levels.ERROR)
+    return vim.cmd "w"
+  end
+
+  -- 创建浮动终端窗口的函数
+  function open_floating_terminal(cmd)
+    local buf = vim.api.nvim_create_buf(false, true) -- 创建一个新的空缓冲区
+    local width = vim.o.columns
+    local height = vim.o.lines
+    local win_height = math.ceil(height * 0.6)
+    local win_width = math.ceil(width * 0.8)
+    local row = math.ceil((height - win_height) / 3)
+    local col = math.ceil((width - win_width) / 2)
+
+    local opts = {
+      style = "minimal",
+      relative = "editor",
+      width = win_width,
+      height = win_height,
+      row = row,
+      col = col,
+      border = "rounded",
+      noautocmd = true,
+    }
+
+    local win = vim.api.nvim_open_win(buf, true, opts)
+
+    if cmd and cmd ~= "" then
+      vim.fn.termopen(cmd)
+      vim.api.nvim_command "startinsert"
+    end
+  end
+
+  -- 执行编译命令（如果存在）
+  if compile_cmd ~= "" then open_floating_terminal(compile_cmd) end
+
+  -- 运行生成的可执行文件或脚本
+  open_floating_terminal(run_cmd)
+end
+
+vim.api.nvim_set_keymap("n", "<F10>", ":lua CompileAndRunFile()<CR>", { noremap = true, silent = true })
+
+--debug configuration
+-- debug for c cpp rust
+local dap = require "dap"
+dap.adapters.gdb = {
+  type = "executable",
+  command = "gdb",
+  args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+}
+dap.configurations.c = {
+  {
+    name = "Launch",
+    type = "gdb",
+    request = "launch",
+    program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end,
+    cwd = "${workspaceFolder}",
+    stopAtBeginningOfMainSubprogram = false,
+  },
+  {
+    name = "Select and attach to process",
+    type = "gdb",
+    request = "attach",
+    program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end,
+    pid = function()
+      local name = vim.fn.input "Executable name (filter): "
+      return require("dap.utils").pick_process { filter = name }
+    end,
+    cwd = "${workspaceFolder}",
+  },
+  {
+    name = "Attach to gdbserver :1234",
+    type = "gdb",
+    request = "attach",
+    target = "localhost:1234",
+    program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end,
+    cwd = "${workspaceFolder}",
+  },
+}
+
+-- debug for golang
+dap.adapters.delve = {
+  type = "server",
+  port = "${port}",
+  executable = {
+    command = "dlv",
+    args = { "dap", "-l", "127.0.0.1:${port}" },
+    -- add this if on windows, otherwise server won't open successfully
+    -- detached = false
+  },
+}
+
+-- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+dap.configurations.go = {
+  {
+    type = "delve",
+    name = "Debug",
+    request = "launch",
+    program = "${file}",
+  },
+  {
+    type = "delve",
+    name = "Debug test", -- configuration for debugging test files
+    request = "launch",
+    mode = "test",
+    program = "${file}",
+  },
+  -- works with go.mod packages and sub packages
+  {
+    type = "delve",
+    name = "Debug test (go.mod)",
+    request = "launch",
+    mode = "test",
+    program = "./${relativeFileDirname}",
+  },
+}
+--debug for js
+require("dap").adapters["pwa-node"] = {
+  type = "server",
+  host = "localhost",
+  port = "${port}",
+  executable = {
+    command = "node",
+    -- 💀 Make sure to update this path to point to your installation
+    args = { "~/js-debug/src/dapDebugServer.js", "${port}" },
+  },
+}
+
+-- clangd config
+local util = require "lspconfig.util"
+
+-- https://clangd.llvm.org/extensions.html#switch-between-sourceheader
+local function switch_source_header(bufnr)
+  bufnr = util.validate_bufnr(bufnr)
+  local clangd_client = util.get_active_client_by_name(bufnr, "clangd")
+  local params = { uri = vim.uri_from_bufnr(bufnr) }
+  if clangd_client then
+    clangd_client.request("textDocument/switchSourceHeader", params, function(err, result)
+      if err then error(tostring(err)) end
+      if not result then
+        print "Corresponding file cannot be determined"
+        return
+      end
+      vim.api.nvim_command("edit " .. vim.uri_to_fname(result))
+    end, bufnr)
+  else
+    print "method textDocument/switchSourceHeader is not supported by any servers active on the current buffer"
+  end
+end
+
+local function symbol_info()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local clangd_client = util.get_active_client_by_name(bufnr, "clangd")
+  if not clangd_client or not clangd_client.supports_method "textDocument/symbolInfo" then
+    return vim.notify("Clangd client not found", vim.log.levels.ERROR)
+  end
+  local params = vim.lsp.util.make_position_params()
+  clangd_client.request("textDocument/symbolInfo", params, function(err, res)
+    if err or #res == 0 then
+      -- Clangd always returns an error, there is not reason to parse it
+      return
+    end
+    local container = string.format("container: %s", res[1].containerName) ---@type string
+    local name = string.format("name: %s", res[1].name) ---@type string
+    vim.lsp.util.open_floating_preview({ name, container }, "", {
+      height = 2,
+      width = math.max(string.len(name), string.len(container)),
+      focusable = false,
+      focus = false,
+      border = require("lspconfig.ui.windows").default_options.border or "single",
+      title = "Symbol Info",
+    })
+  end, bufnr)
+end
+
+local root_files = {
+  ".clangd",
+  ".clang-tidy",
+  ".clang-format",
+  "compile_commands.json",
+  "compile_flags.txt",
+  "configure.ac", -- AutoTools
+}
+
+local default_capabilities = {
+  textDocument = {
+    completion = {
+      editsNearCursor = true,
+    },
+  },
+  offsetEncoding = { "utf-8", "utf-16" },
+}
+
+return {
+  default_config = {
+    cmd = { "clangd", "--compile_commands-dir=build", "xc++", "--std=c++23" },
+    filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+    root_dir = function(fname) return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname) end,
+    single_file_support = true,
+    capabilities = default_capabilities,
+  },
+  commands = {
+    ClangdSwitchSourceHeader = {
+      function() switch_source_header(0) end,
+      description = "Switch between source/header",
+    },
+    ClangdShowSymbolInfo = {
+      function() symbol_info() end,
+      description = "Show symbol info",
+    },
+  },
+  docs = {
+    description = [[
+https://clangd.llvm.org/installation.html
+
+- **NOTE:** Clang >= 11 is recommended! See [#23](https://github.com/neovim/nvim-lsp/issues/23).
+- If `compile_commands.json` lives in a build directory, you should
+  symlink it to the root of your source tree.
+  ```
+  ln -s /path/to/myproject/build/compile_commands.json /path/to/myproject/
+  ```
+- clangd relies on a [JSON compilation database](https://clang.llvm.org/docs/JSONCompilationDatabase.html)
+  specified as compile_commands.json, see https://clangd.llvm.org/installation#compile_commandsjson
+]],
+    default_config = {
+      root_dir = [[
+        root_pattern(
+          '.clangd',
+          '.clang-tidy',
+          '.clang-format',
+          'compile_commands.json',
+          'compile_flags.txt',
+          'configure.ac',
+          '.git'
+        )
+      ]],
+      capabilities = [[default capabilities, with offsetEncoding utf-8]],
+    },
+  },
+}
---
+++ lua/mapping.lua
@@ -0,0 +1,30 @@
+-- Open compiler
+vim.api.nvim_set_keymap("n", "<F6>", "<cmd>CompilerOpen<cr>", { noremap = true, silent = true })
+
+-- Redo last selected option
+vim.api.nvim_set_keymap(
+  "n",
+  "<S-F6>",
+  "<cmd>CompilerStop<cr>" -- (Optional, to dispose all tasks before redo)
+    .. "<cmd>CompilerRedo<cr>",
+  { noremap = true, silent = true }
+)
+
+-- Toggle compiler results
+vim.api.nvim_set_keymap("n", "<S-F7>", "<cmd>CompilerToggleResults<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", ":", "<cmd>FineCmdline<CR>", { noremap = true })
+if pcall(require, "cmake-tools") then
+  vim.api.nvim_set_keymap(
+    "n",
+    "<F4>",
+    "<cmd>wa<CR><cmd>if luaeval('require\"cmake-tools\".is_cmake_project()')|call execute('CMakeRunCurrentFile')|else|call execute('TermExec cmd=./run.sh')|endif<CR>",
+    { noremap = true, silent = true }
+  )
+else
+  vim.api.nvim_set_keymap(
+    "n",
+    "<F4>",
+    "<cmd>wa<CR><cmd>call execute('TermExec cmd=./run.sh')|endif<CR>",
+    { noremap = true, silent = true }
+  )
+end
